117d75574d0ac57a590811c62bafa3bc517a5708
ceciliaphr <cecilia.pinheiro@escolar.ifrn.edu.br>
2025-05-12 15:58:44 -0300

    mudanças

--- POO/Assets/Scenes/Player.cs
+++ POO/Assets/Scenes/Player.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody _rigidbody;
+
+    public float velocidade = 10;
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        Vector3 posicao = transform.position;
+
+        //esquerda X-
+        if (Input.GetKey(KeyCode.A))
+        {
+            posicao.x = posicao.x - velocidade * Time.deltaTime;
+        }
+
+        //direita X+
+        if (Input.GetKey(KeyCode.D))
+        {
+            posicao.x = posicao.x + velocidade * Time.deltaTime;
+        }
+
+        //cima Z+
+        if (Input.GetKey(KeyCode.W))
+        {
+            posicao.z = posicao.z + velocidade * Time.deltaTime;
+        }
+
+        //baixo Z- 
+        if (Input.GetKey(KeyCode.S))
+        {
+            posicao.z = posicao.z - velocidade * Time.deltaTime;
+        }
+
+        transform.position = posicao;
     }
 }
